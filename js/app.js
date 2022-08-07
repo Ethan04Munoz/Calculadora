f7eedc7990309a8b21b737a37d7126ca37d6d070
Ethan04Munoz <ethanmunozlopez04@gmail.com>
2022-08-07 13:13:38 -0500

    Se escriben los numeros en pantalla al clickearlos

--- js/app.js
+++ js/app.js
@@ -1,2 +1,45 @@
+//Pantalla
 const pantallaCalculadora = document.querySelector("#Pantalla");
+
+//Simbolos de arriba
+const porcentajeCalculadora = document.querySelector("#porcentaje");
+const borrarCalculadora = document.querySelector("#borrarC");
+const retrocesoBtnCalculadora = document.querySelector("#retrocesoBtn");
+const unosobrexCalculadora = document.querySelector("#unosobrex");
+const cuadradoCalculadora = document.querySelector("#cuadrado");
+const raizCuadradaCalculadora = document.querySelector("#raizCuadrada");
+
+//Simbolos basicos
+const divisionCalculadora = document.querySelector("#division");
+const multiplicacionCalculadora = document.querySelector("#multiplicacion");
+const restaCalculadora = document.querySelector("#resta");
+const sumaCalculadora = document.querySelector("#suma");
+
+//Numeros
+const ceroCalculadora = document.querySelector("#cero"); 
+const unoCalculadora = document.querySelector("#uno"); 
+const dosCalculadora = document.querySelector("#dos"); 
+const tresCalculadora = document.querySelector("#tres"); 
+const cuatroCalculadora = document.querySelector("#cuatro"); 
+const cincoCalculadora = document.querySelector("#cinco"); 
+const seisCalculadora = document.querySelector("#seis"); 
+const sieteCalculadora = document.querySelector("#siete"); 
+const ochoCalculadora = document.querySelector("#ocho"); 
+const nueveCalculadora = document.querySelector("#nueve"); 
+const puntoCalculadora = document.querySelector("#punto");
+
+//Escribir en la pantalla
+ceroCalculadora.addEventListener("click", () => pantallaCalculadora.textContent = pantallaCalculadora.textContent + "0");
+unoCalculadora.addEventListener("click", () => pantallaCalculadora.textContent = pantallaCalculadora.textContent + "1");
+dosCalculadora.addEventListener("click", () => pantallaCalculadora.textContent = pantallaCalculadora.textContent + "2");
+tresCalculadora.addEventListener("click", () => pantallaCalculadora.textContent = pantallaCalculadora.textContent + "3");
+cuatroCalculadora.addEventListener("click", () => pantallaCalculadora.textContent = pantallaCalculadora.textContent + "4");
+cincoCalculadora.addEventListener("click", () => pantallaCalculadora.textContent = pantallaCalculadora.textContent + "5");
+seisCalculadora.addEventListener("click", () => pantallaCalculadora.textContent = pantallaCalculadora.textContent + "6");
+sieteCalculadora.addEventListener("click", () => pantallaCalculadora.textContent = pantallaCalculadora.textContent + "7");
+ochoCalculadora.addEventListener("click", () => pantallaCalculadora.textContent = pantallaCalculadora.textContent + "8");
+nueveCalculadora.addEventListener("click", () => pantallaCalculadora.textContent = pantallaCalculadora.textContent + "9");
+puntoCalculadora.addEventListener("click", () => pantallaCalculadora.textContent = pantallaCalculadora.textContent + ".");
+
+
 
